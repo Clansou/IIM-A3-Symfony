3c362c0b3cc994c3b7f618b17e30c5e109386d9c
SOUPLET <contact@patinette.io>
2024-07-19 16:57:17 +0200

    Remove Patch and Put from Command

--- src/Entity/Command.php
+++ src/Entity/Command.php
@@ -25,8 +25,6 @@ use ApiPlatform\Doctrine\Orm\Filter\DateFilter;
         new GetCollection(security: "is_granted('ROLE_SERVEUR') or is_granted('ROLE_BARMAN') or is_granted('ROLE_PATRON')"),
         new Get(security: "is_granted('ROLE_SERVEUR') or is_granted('ROLE_BARMAN') or is_granted('ROLE_PATRON')"),
         new Post(name: 'create_command', routeName: 'create_command'),
-        new Put(security: "is_granted('ROLE_SERVEUR') and object.getStatus() != 'payée'", securityMessage: 'You are not allowed to edit this order'),
-        new Patch(security: "is_granted('ROLE_SERVEUR') and object.getStatus() != 'payée'", securityMessage: 'You are not allowed to edit this order'),
         new Patch(name: 'assign_command_to_barman', routeName: 'assign_command_to_barman'),
         new Patch(name: 'command_is_ready', routeName: 'command_is_ready'),
         new Patch(name: 'paid_command', routeName: 'paid_command'),
