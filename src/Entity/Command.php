93be5c0895b07ca0b1c6417b9d19ca88c5b2f655
SOUPLET <contact@patinette.io>
2024-07-19 12:47:56 +0200

    Init project + add entity

---
+++ src/Entity/Command.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommandRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CommandRepository::class)]
+class Command
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $created_date = null;
+
+    /**
+     * @var Collection<int, Drink>
+     */
+    #[ORM\ManyToMany(targetEntity: Drink::class, inversedBy: 'commands')]
+    private Collection $drinks;
+
+    #[ORM\Column]
+    private ?int $table_number = null;
+
+    #[ORM\ManyToOne(inversedBy: 'commands')]
+    private ?User $server = null;
+
+    #[ORM\ManyToOne(inversedBy: 'commands')]
+    private ?User $barman = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $status = null;
+
+    public function __construct()
+    {
+        $this->drinks = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCreatedDate(): ?\DateTimeInterface
+    {
+        return $this->created_date;
+    }
+
+    public function setCreatedDate(\DateTimeInterface $created_date): static
+    {
+        $this->created_date = $created_date;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Drink>
+     */
+    public function getDrinks(): Collection
+    {
+        return $this->drinks;
+    }
+
+    public function addDrink(Drink $drink): static
+    {
+        if (!$this->drinks->contains($drink)) {
+            $this->drinks->add($drink);
+        }
+
+        return $this;
+    }
+
+    public function removeDrink(Drink $drink): static
+    {
+        $this->drinks->removeElement($drink);
+
+        return $this;
+    }
+
+    public function getTableNumber(): ?int
+    {
+        return $this->table_number;
+    }
+
+    public function setTableNumber(int $table_number): static
+    {
+        $this->table_number = $table_number;
+
+        return $this;
+    }
+
+    public function getServer(): ?User
+    {
+        return $this->server;
+    }
+
+    public function setServer(?User $server): static
+    {
+        $this->server = $server;
+
+        return $this;
+    }
+
+    public function getBarman(): ?User
+    {
+        return $this->barman;
+    }
+
+    public function setBarman(?User $barman): static
+    {
+        $this->barman = $barman;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Drink.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DrinkRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: DrinkRepository::class)]
+class Drink
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?float $price = null;
+
+    #[ORM\OneToOne(cascade: ['persist', 'remove'])]
+    private ?Media $image = null;
+
+    /**
+     * @var Collection<int, Command>
+     */
+    #[ORM\ManyToMany(targetEntity: Command::class, mappedBy: 'drinks')]
+    private Collection $commands;
+
+    public function __construct()
+    {
+        $this->commands = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getImage(): ?Media
+    {
+        return $this->image;
+    }
+
+    public function setImage(?Media $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Command>
+     */
+    public function getCommands(): Collection
+    {
+        return $this->commands;
+    }
+
+    public function addCommand(Command $command): static
+    {
+        if (!$this->commands->contains($command)) {
+            $this->commands->add($command);
+            $command->addDrink($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCommand(Command $command): static
+    {
+        if ($this->commands->removeElement($command)) {
+            $command->removeDrink($this);
+        }
+
+        return $this;
+    }
+}
